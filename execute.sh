fff50a2543dddad1cb7ae703834aa357830d4ad1
Adeel Kazmi <adeel.kazmi@samsung.com>
2019-10-02 16:35:22 +0100

    Updated the execute.sh script
    1) Verbosity reduced by default, can be enabled via an option
    2) Added colors
    
    Change-Id: I579fe588e86de1882f1927921ac6a70583d32aa4

--- execute.sh
+++ execute.sh
@@ -1,5 +1,55 @@
 #!/bin/bash
 
+# Usage Function
+Usage()
+{
+  echo "Usage: $(basename ${BASH_SOURCE[0]}) [OPTIONS]"
+  echo " Optional Options:"
+  # Just do a simple grep of this file so we can keep the command with the comment together
+  grep ". )\ #" ${BASH_SOURCE[0]} | sed 's/# //' | awk -F ")" '{ printf "%-25s %s\n", $1, $2 }'
+  exit 0
+}
+
+# Initialise the options
+OPTS=$(getopt -o vh --long verbose,help -n "$(basename "$0")" -- "$@")
+if [ $? != 0 ]; then echo; Usage; fi
+eval set -- "$OPTS"
+
+REDIRECT_OUTPUT="> /dev/null 2>&1"
+
+# Go through all the options
+if [[ $* > 1 ]] ; then
+  while true;
+  do
+    case "$1" in
+      -v|--verbose ) # Verbose output for every test case
+        REDIRECT_OUTPUT=
+        shift
+        ;;
+
+      -h|--help ) # Help
+        shift
+        Usage
+        ;;
+
+      -- )
+        shift
+        ;;
+
+    * )
+      break
+      ;;
+
+    esac
+  done
+fi
+
+# Formatting Codes
+Bold="\e[1m"
+Green='\e[0;32m'
+Red='\e[0;31m'
+Clear='\e[0m'
+
 # Retrieve all the installed tests
 tests=$( ls -d $DESKTOP_PREFIX/bin/*.test )
 num_tests=${#tests[@]}
@@ -9,8 +59,9 @@ num_fails=0
 # Execute each test executable in turn
 for i in $tests ; do
   test=$(basename $i)
-  echo Executing: $test
-  $i
+  echo -e "${Bold}Executing: $test${Clear}"
+  command="$i ${REDIRECT_OUTPUT}"
+  eval $command
 
   # Check the test result
   if [ "$?" != "0" ]; then
@@ -23,8 +74,12 @@ for i in $tests ; do
 done
 
 # Output the summary of test result
-echo "Test Summary:"
-echo "Total tests: $num_tests"
-echo "$num_passes passed"
-echo "$num_fails failed"
+TestOutputColor=${Green}
+if [[ ! "$num_passes" = "$num_tests" ]] ; then TestOutputColor=${Red}; fi
+percent_passing=$(printf %.2f\\n "$((10000 * $num_passes / $num_tests ))e-2")
+echo
+echo -e "${Bold}Test Summary:${Clear}"
+echo -e "  Total tests: $num_tests"
+echo -e "  Number of test passes: ${Bold}$num_passes ($percent_passing%)${Clear}"
+echo -e "  ${TestOutputColor}Number of test failures: ${Bold}$num_fails ${Clear}"
 
